7624fd4cf3c29c41caa695d55b96e5e5abde4e57
Sleepy <biegacz1993@wp.pl>
2017-11-12 15:02:54 +0100

    profile changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,16 @@
 class UsersController < ApplicationController
-  respond_to :html, :js
+  helper_method :render_profile
 
   expose(:users)
   expose_decorated(:user) { User.find(params[:id]) }
   expose_decorated(:profile) { User.find(params[:id]).profile }
   expose_decorated(:address) { User.find(params[:id]).address }
 
-  def render_text_input
-    
+  def render_profile(name, field_name, field_value, record_name)
+    view_context.render partial: 'users/profile/text_input_content',
+                        locals: { name: name,
+                                  field_name: field_name,
+                                  field_value: field_value,
+                                  record_name: record_name }
   end
 end
